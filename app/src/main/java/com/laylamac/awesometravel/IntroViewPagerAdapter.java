c04cad124656e1c377c62f6547b8110d7f103a1a
laylamac <laylas.stickearn@gmail.com>
2019-03-09 17:50:07 +0700

    Intro Screen

---
+++ app/src/main/java/com/laylamac/awesometravel/IntroViewPagerAdapter.java
@@ -0,0 +1,63 @@
+package com.laylamac.awesometravel;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+class IntroViewPagerAdapter extends PagerAdapter {
+
+
+    Context mContext;
+    List<ScreenItem> mListScreen;
+
+    public IntroViewPagerAdapter(Context mContext, List<ScreenItem> mListScreen) {
+        this.mContext = mContext;
+        this.mListScreen = mListScreen;
+    }
+
+
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@Nullable ViewGroup container, int position) {
+
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layoutScreen = inflater.inflate(R.layout.layout_screen, null);
+
+        ImageView imgSlide = layoutScreen.findViewById(R.id.intro_img);
+        TextView title = layoutScreen.findViewById(R.id.intro_title);
+        TextView decription = layoutScreen.findViewById(R.id.intro_descrtiption);
+
+        title.setText(mListScreen.get(position).getTitle());
+        decription.setText(mListScreen.get(position).getDescription());
+        imgSlide.setImageResource(mListScreen.get(position).getScreenImg());
+
+        container.addView(layoutScreen);
+        return layoutScreen;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == o;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public int getCount() {
+        return mListScreen.size();
+    }
+
+
+}
